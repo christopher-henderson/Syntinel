35a1f6477c1acb23ffb7095d462c66d194ca1bd4
christopher-henderson <chris@chenderson.org>
2016-12-03 12:43:39 -0700

    added all of the urls for our current models

--- compozed/syntinel/api/v1/urls.py
+++ compozed/syntinel/api/v1/urls.py
@@ -3,5 +3,18 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^docker/(?P<pk>[0-9]+)?/?$', DockerView.as_view(), name='docker')
+    url(r'^docker/(?P<pk>[0-9]+)?/?$', DockerView.as_view(), name='docker'),
+    url(r'^docker/all/?$', DockerListView.as_view(), name='docker_list'),
+
+    url(r'^test/(?P<pk>[0-9]+)?/?$', TestView.as_view(), name='test'),
+    url(r'^test/all/?$', TestListView.as_view(), name='test_list'),
+
+    url(r'^suite/(?P<pk>[0-9]+)?/?$', SuiteView.as_view(), name='suite'),
+    url(r'^suite/all/?$', SuiteListView.as_view(), name='suite_list'),
+
+    url(r'^script/(?P<pk>[0-9]+)?/?$', ScriptView.as_view(), name='script'),
+    url(r'^script/all/?$', ScriptListView.as_view(), name='script_list'),
+
+    url(r'^testrun/(?P<pk>[0-9]+)?/?$', TestRunView.as_view(), name='testrun'),
+    url(r'^testrun/all/?$', TestRunListView.as_view(), name='testrun_list'),
     ]
