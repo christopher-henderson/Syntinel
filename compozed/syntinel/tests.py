a1057e7b88e09b8d5d4485f1cd178ddac8d42354
christopher-henderson <chris@chenderson.org>
2016-12-04 10:07:15 -0700

    fleshing out tests

--- compozed/syntinel/tests.py
+++ compozed/syntinel/tests.py
@@ -1,7 +1,10 @@
+import json
+
 from django.test import TestCase
 from django.test import Client
 
 from .api.v1.models import Docker
+from .api.v1.serializers import DockerSerializer
 
 client = Client()
 
@@ -24,9 +27,46 @@ class DockerTestCase(TestCase):
         self.assertEqual(junit.platform, 'RHEL7')
         self.assertEqual(selenium.platform, 'Ubuntu')
 
+    def test_docker_get_all(self):
+        objs = Docker.objects.all()
+        self.assertGreater(len(objs), 0)
+
     def test_docker_client_post(self):
-        response = client.post('/api/v1/docker/', data={'name':'Test', 'platform':'RHEL7'})
+        response = client.post(
+            '/api/v1/docker/', data={'name': 'Test', 'platform': 'RHEL7'})
         self.assertEqual(response.status_code, 201)
+
+    def test_docker_client_patch(self):
+        response = client.post(
+            '/api/v1/docker/', data={'name': 'Test', 'platform': 'RHEL7'})
+        self.assertEqual(response.status_code, 201)
+        obj = json.loads(response.content.decode())
+        response = client.patch(
+            '/api/v1/docker/{ID}'.format(ID=obj['id']),
+            data=json.dumps({"name": "Updated"}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        updated_obj = json.loads(response.content.decode())
+        self.assertEqual(updated_obj['name'], 'Updated')
+        db_obj = Docker.objects.get(id=updated_obj['id'])
+        self.assertEqual(updated_obj['name'], db_obj.name)
+
+    def test_docker_client_put(self):
+        response = client.post(
+            '/api/v1/docker/', data={'name': 'Test', 'platform': 'RHEL7'})
+        self.assertEqual(response.status_code, 201)
+        obj = json.loads(response.content.decode())
+        response = client.put(
+            '/api/v1/docker/{ID}'.format(ID=obj['id']),
+            data=json.dumps({'name': 'Updated', 'platform': 'Totally new'}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        updated_obj = json.loads(response.content.decode())
+        self.assertEqual(updated_obj['name'], 'Updated')
+        self.assertEqual(updated_obj['platform'], 'Totally new')
+        db_obj = Docker.objects.get(id=updated_obj['id'])
+        self.assertEqual(updated_obj['name'], db_obj.name)
+        self.assertEqual(updated_obj['platform'], db_obj.platform)
 
     def test_docker_client_get(self):
         rhel = Docker.objects.get(platform='RHEL7')
