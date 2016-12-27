56ee4d94c936d307be400a459b77bf4439a0fb6f
christopher-henderson <chris@chenderson.org>
2016-12-26 20:21:17 -0700

    added race condition test

--- syntinel_executor/src/tests/process_test.go
+++ syntinel_executor/src/tests/process_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"syntinel_executor/process"
 	"testing"
+	"time"
 )
 
 func TestProcess(t *testing.T) {
@@ -54,4 +55,32 @@ func TestProcessBadInvocationKill(t *testing.T) {
 	if output.Err == nil {
 		t.Errorf("No error on bad invocation.")
 	}
+}
+
+// There's no assertion on this one. Rather it is intentionally trying to
+// send a kill signal at the exact same time the process completes in the hopes
+// it reveals a panic. Turns out that it is INCREDIBLY unlikely, if even possible.
+//
+// The typical results are one of either:
+// 	process_test.go:84: <nil>
+// 	process_test.go:85: Done.
+//
+// 	process_test.go:84: killed
+// 	process_test.go:85: Done.
+//
+// 	process_test.go:84: killed
+// 	process_test.go:85:
+//
+// All of which seem fine.
+func TestProcessRace(t *testing.T) {
+	command := "python"
+	args := []string{"-c", "from time import sleep;sleep(.01);print('Done.')"}
+	proc, result, cancel := process.NewProcess(command, args...)
+	defer close(cancel)
+	proc.Start()
+	time.Sleep(time.Millisecond * 33)
+	cancel <- 1
+	output := <-result
+	t.Log(output.Err)
+	t.Log(output.Output)
 }
