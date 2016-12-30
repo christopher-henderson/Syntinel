c477c73e8c853227da3552d4c5092441df1a84b4
christopher-henderson <chris@chenderson.org>
2016-12-30 11:26:49 -0700

    marshalling responsiblities moved out of Payload type

--- syntinel_executor/src/controller/payload.go
+++ syntinel_executor/src/controller/payload.go
@@ -1,19 +1,6 @@
 package controller
 
-import (
-	"encoding/json"
-	"log"
-)
-
 type Payload struct {
 	Error error       `json:"error"`
 	Data  interface{} `json:"data"`
-}
-
-func NewPayload(requestError error, data interface{}) []byte {
-	payload, err := json.MarshalIndent(Payload{requestError, data}, "", "    ")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return payload
 }
