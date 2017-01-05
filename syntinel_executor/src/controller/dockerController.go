4ff27e8d389c9f8bc29ec176fe505bac69c15051
christopher-henderson <chris@chenderson.org>
2017-01-05 08:32:43 -0700

    added comments on parsing the multiform in the Docker controller

--- syntinel_executor/src/controller/dockerController.go
+++ syntinel_executor/src/controller/dockerController.go
@@ -19,6 +19,10 @@ func RegisterDocker(w http.ResponseWriter, r *http.Request) {
 		payload.Data = "Bad Docker ID."
 		return
 	}
+	// Zero in http.Request.ParseMultipartForm means "do not save to memory,
+	// and dump the whole thing to a temporary file on disk". Which is convenient
+	// because we want to get this out of memory and onto the file system
+	// as fast as possible. Once in the temp file we can link it the original.
 	r.ParseMultipartForm(0)
 	data, header, _ := r.FormFile("docker")
 	defer data.Close()
