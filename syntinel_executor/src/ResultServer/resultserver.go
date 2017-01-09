3d392653b24c4dddca5cf4b63dc24f3be87a1b7a
christopher-henderson <chris@chenderson.org>
2017-01-09 16:59:23 -0700

    taking a crack at a backoff strategy.

--- syntinel_executor/src/ResultServer/resultserver.go
+++ syntinel_executor/src/ResultServer/resultserver.go
@@ -2,18 +2,26 @@ package ResultServer
 
 import (
 	"log"
+	"sync"
 	"syntinel_executor/PAO/process"
+	"time"
 )
+
+const maxBackoff = 20
 
 var rs = newResultServer()
 
 type resultServer struct {
-	in chan *process.TestRunResult
+	in      chan *process.TestRunResult
+	retry   chan *process.TestRunResult
+	mutex   sync.Mutex
+	backoff int
 }
 
 func newResultServer() *resultServer {
-	rs := &resultServer{make(chan *process.TestRunResult)}
+	rs := &resultServer{make(chan *process.TestRunResult), make(chan *process.TestRunResult), sync.Mutex{}, 0}
 	go rs.ListenForResults()
+	go rs.ListenForRetries()
 	return rs
 }
 
@@ -34,6 +42,34 @@ func (rs *resultServer) ListenForResults() {
 	}
 }
 
+func (rs *resultServer) ListenForRetries() {
+	for {
+		go rs.handle(<-rs.retry)
+		time.Sleep(time.Second * time.Duration(rs.backoff))
+	}
+}
+
 func (rs *resultServer) handle(result *process.TestRunResult) {
 	log.Println("The Result Server got the following result: ", result)
+	succeeded := true
+	if !succeeded {
+		rs.mutex.Lock()
+		rs.backoff += 1
+		if rs.backoff < maxBackoff {
+			rs.backoff = maxBackoff
+		}
+		rs.mutex.Unlock()
+		rs.enqueue(result)
+	} else {
+		rs.mutex.Lock()
+		rs.backoff -= 1
+		if rs.backoff < 0 {
+			rs.backoff = 0
+		}
+		rs.mutex.Unlock()
+	}
+}
+
+func (rs *resultServer) enqueue(result *process.TestRunResult) {
+
 }
