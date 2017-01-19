d47c8a31b5e73596a353a5fcebbe325d55f4637e
christopher-henderson <chris@chenderson.org>
2017-01-19 09:20:09 -0700

    we'll delete this later

---
+++ syntinel_executor/test.sh
@@ -0,0 +1,14 @@
+# curl -X POST http://localhost:8000/docker/1 --data-binary @/Users/chris/Downloads/centos.iso
+
+# curl -X POST http://localhost:8000/docker/1 -F "docker=@/Users/chris/Downloads/centos.iso"
+# curl -X POST http://localhost:8000/docker/1 -F "docker=@/Users/chris/Downloads/centos.iso"
+# curl -X POST http://localhost:8000/docker/1 -F "docker=@/Users/chris/Downloads/centos.iso"
+
+curl -X POST http://192.168.1.2:8000/script/1 -d "#!/usr/bin/env bash
+git clone https://github.com/christopher-henderson/TestTheTester.git && cd TestTheTester/GoBeInGoodHands && go test . -v -cover"
+curl -X POST http://192.168.1.2:8000/docker/1 -F "docker=@/Users/chris/Documents/ASU/Syntinel/syntinel_executor/Dockerfile"
+curl -X POST http://192.168.1.2:8000/test/1?dockerID=1\&scriptID=1
+curl -X POST http://192.168.1.2:8000/test/run?testID=1\&testRunID=2
+# sleep 2s
+# curl -X DELETE http://localhost:8000/test/run?testID=1\&testRunID=2
+# curl -X GET http://localhost:8000/test/run?testID=1\&testRunID=2
