6a09573cea4b8f6d29be1bb71a7825dda3e1bd6b
christopher-henderson <chris@chenderson.org>
2017-01-19 10:03:02 -0700

    more thorough tests

--- syntinel_executor/test.sh
+++ syntinel_executor/test.sh
@@ -9,6 +9,18 @@ git clone https://github.com/christopher-henderson/TestTheTester.git && cd TestT
 curl -X POST http://192.168.1.2:8000/docker/1 -F "docker=@/Users/chris/Documents/ASU/Syntinel/syntinel_executor/Dockerfile"
 curl -X POST http://192.168.1.2:8000/test/1?dockerID=1\&scriptID=1
 curl -X POST http://192.168.1.2:8000/test/run?testID=1\&testRunID=2
+curl -X POST http://192.168.1.2:8000/test/run?testID=1\&testRunID=3
+curl -X POST http://192.168.1.2:8000/test/run?testID=1\&testRunID=4
+
+curl -X POST http://192.168.1.2:8000/test/2?dockerID=1\&scriptID=1
+curl -X POST http://192.168.1.2:8000/test/run?testID=2\&testRunID=2
+curl -X POST http://192.168.1.2:8000/test/run?testID=2\&testRunID=3
+curl -X POST http://192.168.1.2:8000/test/run?testID=2\&testRunID=4
+
+curl -X POST http://192.168.1.2:8000/test/3?dockerID=1\&scriptID=1
+curl -X POST http://192.168.1.2:8000/test/run?testID=3\&testRunID=2
+curl -X POST http://192.168.1.2:8000/test/run?testID=3\&testRunID=3
+curl -X POST http://192.168.1.2:8000/test/run?testID=3\&testRunID=4
 # sleep 2s
 # curl -X DELETE http://localhost:8000/test/run?testID=1\&testRunID=2
 # curl -X GET http://localhost:8000/test/run?testID=1\&testRunID=2
