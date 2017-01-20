888721c636fa9f91dbe5d8b2f4565bab8ec49b7b
christopher-henderson <chris@chenderson.org>
2017-01-19 19:13:58 -0700

    refactor of utl functions

--- syntinel_executor/src/DAO/docker.go
+++ syntinel_executor/src/DAO/docker.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"syntinel_executor/utils"
 )
 
 type Docker struct {
@@ -17,7 +18,15 @@ func (d *Docker) Save(data io.Reader) {
 	absolutePath := absDockerPath()
 	path := absolutePath + id
 	tmpPath := absolutePath + "." + id
-	defer cleanup(path, tmpPath)
+	defer func() {
+		if err := recover(); err != nil {
+			// If there was an error, attempt to move the original (not 'tmp')
+			// back to where it was.
+			utils.FileCopy(tmpPath, path)
+			utils.FileRemove(tmpPath)
+			panic(err)
+		}
+	}()
 	dst, err := os.Create(path)
 	if err != nil {
 		log.Fatalln(err)
@@ -26,14 +35,14 @@ func (d *Docker) Save(data io.Reader) {
 	if _, err := io.Copy(dst, data); err != nil {
 		log.Fatalln(err)
 	}
-	if err := remove(tmpPath); err != nil {
+	if err := utils.FileRemove(tmpPath); err != nil {
 		log.Fatalln(err)
 	}
 }
 
 func (d *Docker) Delete() {
 	path := absDockerPath() + strconv.Itoa(d.ID)
-	if err := remove(path); err != nil {
+	if err := utils.FileRemove(path); err != nil {
 		log.Fatalln(err)
 	}
 }
