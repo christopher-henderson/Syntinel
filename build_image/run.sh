af47729a7e5223f0311b396c3b2aaf5d893ad658
christopher-henderson <chris@chenderson.org>
2017-02-18 16:58:26 -0700

    docker container and supervisord == win

---
+++ build_image/run.sh
@@ -0,0 +1,1 @@
+docker run --name syntinel --detach -v /home/chris/Syntinel:/opt/Syntinel syntinel 
--- compozed/compozed/settings.py
+++ compozed/compozed/settings.py
@@ -55,11 +55,10 @@ INSTALLED_APPS = [
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "asgiref.inmemory.ChannelLayer",
-        # "BACKEND": "asgi_redis.RedisChannelLayer",
-        # "CONFIG": {
-        #     "hosts": [("localhost", 6379)],
-        #     },
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],
+            },
         "ROUTING": "compozed.routing.channel_routing",
         },
     }
