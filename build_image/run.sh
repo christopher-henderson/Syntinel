ab62d88709de42adacd5f0b3fa3c240c0202bc1e
christopher-henderson <chris@chenderson.org>
2017-02-18 21:38:13 -0700

    more devopsy stuff

--- build_image/run.sh
+++ build_image/run.sh
@@ -1,1 +1,2 @@
-docker run --name syntinel --detach -v /home/chris/Syntinel:/opt/Syntinel syntinel 
+#docker run --name syntinel --detach -v /home/chris/Syntinel:/opt/Syntinel syntinel
+docker run --name syntinel -it --rm -v /home/chris/Syntinel:/opt/Syntinel syntinel /bin/bash
