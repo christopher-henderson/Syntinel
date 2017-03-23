c972dcde9868bb77100efb7e27cef087a1973856
David Henderson <therealdavidhenderson@gmail.com>
2017-03-22 17:39:36 -0700

    Added work on populating index.html

--- bootstrap/js/syntinel-utils.js
+++ bootstrap/js/syntinel-utils.js
@@ -1,16 +1,34 @@
+var SYNTINEL_URL = "http://syntinel.chenderson.org/api/v1";
+
 function getQueryVariable(variable) {
-   var query = window.location.search.substring(1);
-   var vars = query.split("&");
-   for (var i=0;i<vars.length;i++) {
-           var pair = vars[i].split("=");
-           if(pair[0] == variable){return pair[1];}
-   }
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		if(pair[0] == variable){return pair[1];}
+	}
 }
 
 function escapeNewLineChars(valueToEscape) {
-   if (valueToEscape != null && valueToEscape != "") {
-      return valueToEscape.replace(/\n/g, "\\n");
-   } else {
-      return valueToEscape;
-   } 
+	if (valueToEscape != null && valueToEscape != "") {
+		return valueToEscape.replace(/\n/g, "\\n");
+	} else {
+		return valueToEscape;
+	}
+}
+
+function apiGet(url, params, callback) {
+	var handler = function(request) {
+		callback(request);
+	};
+
+	var request = new XMLHttpRequest();
+	request.open("GET", url, true);
+	request.withCredentials = true;
+	request.setRequestHeader("Content-Type","application/json");
+	request.send();
+	request.onreadystatechange = function() {
+		if(request.readyState >= 4)
+			handler(request);
+	};
 }
