13229ae9395d057a13fdb04ec6d1ec429a7d35a4
David Henderson <therealdavidhenderson@gmail.com>
2017-03-29 16:42:22 -0700

    Added Test page linkages

--- bootstrap/js/syntinel-project.js
+++ bootstrap/js/syntinel-project.js
@@ -1,10 +1,64 @@
 function pageLoad() {
+	var project = {};
+	var t = [];
+
 	var projectID = getQueryVariable("project");
 
-	var add = document.getElementById("button-test-add");
-	add.addEventListener('click', function() {
+	document.getElementById("button-test-add").addEventListener('click', function() {
 		// Open the modal
 		$("#modal-add").modal();
 	});
 
+	var createProject = document.getElementById("modal-add-test").addEventListener('click', function() {
+		var test = {};
+		window.location="test.html?project="+projectID+"&test="+test.id;
+	});
+
+	var populatePage = function() {
+		var pageHeader = document.getElementById("header-project-name");
+		pageHeader.innerHTML = project.name + " <small>Syntinel Project</small>";
+
+		var breadcrumbProject = document.getElementById("breadcrumb-project-name");
+		breadcrumbProject.innerHTML = "<i class=\"fa fa-file\"></i> " + project.name;
+
+		var projectTests = document.getElementById("table-project-tests-body");
+		projectTests.innerHTML = "";
+		for(var i = 0; i < t.length; i++) {
+			var test = t[i];
+			var testRow = "";
+
+			testRow += "<tr class=\"" + (test.health >= 100 ? "success" : (test.health > 50 ? "warning" : "danger")) + "\">";
+			testRow += "	<td>" + test.id + "</td>";
+			testRow += "	<td>" + test.name + "</td>";
+			testRow += "	<td>" + test.health + "%</td>";
+			testRow += "</tr>";
+
+			projectTests.innerHTML += testRow;
+		}
+	}
+
+	// Make all the calls
+	apiGet(SYNTINEL_URL + "/project/" + projectID, "", function(res) {
+		project = res;
+		project = escapeNewLineChars(project);
+		project = JSON.parse(project);
+
+		var count = 0;
+		for(var j = 0; j < project.tests.length; j++) {
+			apiGet("/test/" + projectID, "", function(res) {
+				t.push(JSON.parse(escapeNewLineChars(res)));
+				count++;
+				if(count == project.tests.length) {
+					populatePage();
+				}
+			});
+		}
+	});
+
+	$('#table-project-tests').find('tr').click(function() {
+		var index = ($(this).index());
+		var row = document.getElementById("table-project-tests-body").childNodes[index];
+		var id = row.childNodes[1].innerHTML;
+		window.location = "test.html?project="+projectID+"&test="+id;
+	});
 }
--- bootstrap/js/syntinel-test.js
+++ bootstrap/js/syntinel-test.js
@@ -1,40 +1,19 @@
 function pageLoad() {
+	var project = {};
+	var test = {};
+	var r = [];
+
 	var projectID = getQueryVariable("project");
 	var testID = getQueryVariable("test");
-	var test;
-	if(typeof testID !== "undefined") {
-		// TODO API call to get current test
-		test = "{\"id\":1,\"name\":\"The greatest song in the world\",\"script\":\"#!/usr/bin/env bash\ngit clone https://github.com/christopher-henderson/TestTheTester.git && cd TestTheTester/GoBeInGoodHands && go test . -v -cover\",\"dockerfile\":\"FROM docker.io/centos\n\nMAINTAINER Christopher Henderson\n\nRUN yum install -y go git wget\nCOPY script.sh $HOME/script.sh\nCMD chmod +x script.sh && ./script.sh\",\"environmentVariables\":[\"a=b\"],\"health\":100,\"suite\":null}";
-		test = escapeNewLineChars(test);
-		try {
-			test = JSON.parse(test);
-		} catch(err) {
-			console.log(err);
-		}
-	}
 
-	var projects = [
-		{name : "CompoZed"}
-	]
-
-	// Setting - Project dropdown
-	var projectNames = "";
-	for(var i = 0; i < projects.length; i++) {
-		projectNames += "<option>" + projects[i].name + "</option>";
-	}
-	if(projectNames.length == 0) {
-		projectNames += "<option>No project selected</option>";
-	}
-	document.getElementById("setting-project").innerHTML = projectNames;
-
-	if(typeof test === "undefined") {
-		// Page header
-		document.getElementById("header-test-name").innerHTML = "Create new test <small>Syntinel Test</small>";
-		document.getElementById("breadcrumb-test-name").innerHTML = "<i class=\"fa fa-file\"></i> Create new test";
-	} else {
+	var populatePage = function() {
 		// Page header
 		document.getElementById("header-test-name").innerHTML = test.name + " <small>Syntinel Test</small>";
+		document.getElementById("breadcrumb-project-name").innerHTML = "<i class=\"fa fa-file\"></i> <a href=\"project.html?project=" + projectID + "\">" + project.name + "</a>";
 		document.getElementById("breadcrumb-test-name").innerHTML = "<i class=\"fa fa-file\"></i> " + test.name;
+
+		// Setting - Name
+		document.getElementById("setting-project").value = projectID;
 
 		// Setting - Name
 		document.getElementById("setting-testName").value = test.name;
@@ -59,5 +38,64 @@ function pageLoad() {
 			envStr += "</td></tr>";
 		}
 		document.getElementById("setting-environmentVariables").innerHTML = envStr;
+
+		// Run histories
+		var testRuns = document.getElementById("table-test-runs-body");
+		testRuns.innerHTML = "";
+		for(var i = 0; i < r.length; i++) {
+			var run = r[i];
+			var runRow = "";
+
+			var runStatus;
+			if(!run.successful || run.successful == null) {
+				runStatus = "Running";
+			} else if(run.successful == true) {
+				runStatus = "Successful";
+			} else {
+				runStatus = "Failed";
+			}
+
+			runRow += "<tr class=\"" + (runStatus == "Successful" ? "success" : (runStatus == "Running" ? "warning" : "danger")) + "\">";
+			runRow += "	<td>" + run.id + "</td>";
+			runRow += "	<td>" + runStatus + "</td>";
+			runRow += "</tr>";
+
+			testRuns.innerHTML += runRow;
+		}
 	}
+
+	// Make all the calls
+	apiGet(SYNTINEL_URL + "/project/" + projectID, "", function(res) {
+		project = res;
+		project = escapeNewLineChars(project);
+		project = JSON.parse(project);
+
+		// Now get the test
+		apiGet(SYNTINEL_URL + "/test/" + testID, "", function(res) {
+			test = res;
+			test = escapeNewLineChars(test);
+			test = JSON.parse(test);
+
+			// Get test runs
+			apiGet(SYNTINEL_URL + "/testrun/all?test=" + testID, "", function(res) {
+				var runs = res;
+				runs = escapeNewLineChars(runs);
+				runs = JSON.parse(runs);
+
+				for(var i = 0; i < runs.length; i++) {
+					var run = runs[i];
+					r.push(run);
+				}
+
+				populatePage();
+			});
+		});
+	});
+
+	$('#table-test-runs').find('tr').click(function() {
+		var index = ($(this).index());
+		var row = document.getElementById("table-test-runs-body").childNodes[index];
+		var id = row.childNodes[1].innerHTML;
+		window.location = "run.html?project="+projectID+"&test="+testID+"&run="+id;
+	});
 }
